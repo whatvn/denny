cad61224abc5a6b3786250ba0f72e7c06bb51fd7
Nguyễn Văn Hưng <hungnv4@vng.com.vn>
2019-08-02 18:59:25 +0700

    support set log formatter when init log

--- log/log.go
+++ log/log.go
@@ -14,13 +14,23 @@ type Log struct {
 	step int32
 }
 
-func New() *Log {
+type TextFormatter = logrus.TextFormatter
+type JSONFormatter = logrus.JSONFormatter
+type Formatter = logrus.Formatter
+
+// New return a new log object with log start time
+func New(formatter ...Formatter) *Log {
+	if len(formatter) > 0 {
+		logrus.SetFormatter(formatter[0])
+	}
 	logTime := time.Now().Format(time.RFC3339)
 	return &Log{
 		Entry: logrus.WithField("Time", logTime),
 	}
 }
 
+// ToJsonString convert an object into json string to beautify log
+// return nil if marshalling error
 func (l *Log) ToJsonString(input interface{}) string {
 	if bytes, err := json.Marshal(input); err == nil {
 		return string(bytes)
@@ -34,6 +44,7 @@ func (l *Log) addStep() {
 	l.step += 1
 }
 
+// AddLog add a new field to log with step = current step + 1
 func (l *Log) AddLog(line string, format ...interface{}) {
 	l.addStep()
 	step := fmt.Sprintf("STEP_%d", l.step)
@@ -45,10 +56,12 @@ func (l *Log) AddLog(line string, format ...interface{}) {
 	l.Entry = l.Entry.WithField(step, line)
 }
 
+// WithField a a new key = value to log with key = field, value = value
 func (l *Log) WithField(field string, value interface{}) {
 	l.Entry = l.Entry.WithField(field, value)
 }
 
+// WithFields add multiple key/value to log: key1 = value1, key2 = value2
 func (l *Log) WithFields(fields map[string]interface{}) {
 	l.Entry = l.Entry.WithFields(fields)
 }
