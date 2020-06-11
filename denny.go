6409c426716d64976fa1fe578263396092aeed13
whatvn <hungnv@opensource.com.vn>
2020-06-11 15:26:45 +0700

    register shutdown hook

--- denny.go
+++ denny.go
@@ -13,9 +13,12 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
 	"reflect"
 	"strings"
 	"sync"
+	"syscall"
+	"time"
 )
 
 type Context = gin.Context
@@ -378,22 +381,26 @@ func (r *Denny) GraceFulStart(addrs ...string) error {
 		}()
 	}
 
-	//// Wait for interrupt signal to gracefully shutdown the server with
-	//// a timeout of 5 seconds.
-	//quit := make(chan os.Signal, 1)
-	//// kill (no param) default send syscanll.SIGTERM
-	//// kill -2 is syscall.SIGINT
-	//// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
-	//signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	//<-quit
-	//r.Infof("Shutdown Server ...")
-	//
-	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	//defer cancel()
-	//if err := httpSrv.Shutdown(ctx); err != nil {
-	//	r.Fatal("Server Shutdown: ", err)
-	//	return err
-	//}
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscanll.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	r.Infof("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if r.grpcServer != nil {
+		r.grpcServer.GracefulStop()
+	}
+
+	if err := httpSrv.Shutdown(ctx); err != nil {
+		r.Fatal("Server Shutdown: ", err)
+		return err
+	}
 	return nil
 }
 
