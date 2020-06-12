f94d59a3ca5c0eb714f73a836d9785d4ec53df91
whatvn <hungnv@opensource.com.vn>
2020-06-12 18:05:47 +0700

    report request ip in log, also grpc status string instead of number

--- middleware/grpc/logger.go
+++ middleware/grpc/logger.go
@@ -5,24 +5,29 @@ import (
 	"github.com/whatvn/denny/log"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 	"time"
 )
 
 func LoggerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-	logger := log.New(&log.JSONFormatter{})
-	start := time.Now()
-	panicking := true
+	var (
+		logger    = log.New(&log.JSONFormatter{})
+		start     = time.Now()
+		panicking = true
+	)
 
+	p, ok := peer.FromContext(ctx)
+	if ok {
+		logger.WithField("request_ip", p.Addr.String())
+	}
 	logger.WithFields(map[string]interface{}{
-		"start": start,
-		"Uri":   info.FullMethod,
+		"start":   start,
+		"uri":     info.FullMethod,
+		"request": logger.ToJsonString(req),
 	})
 
 	defer func() {
-		// We don't want to recover anything, but we want to log Internal error
-		// in case of a panic. We pray here reportServerRPCMetrics is very
-		// lightweight and it doesn't panic itself.
 		code := codes.OK
 		switch {
 		case err != nil:
@@ -30,8 +35,8 @@ func LoggerInterceptor(ctx context.Context, req interface{}, info *grpc.UnarySer
 		case panicking:
 			code = codes.Internal
 		}
-		logger.WithField("code", code)
-		logger.Infof("Finish in %v", time.Now().Sub(start))
+		logger.WithField("code", code.String())
+		logger.Infof("latency: ", time.Now().Sub(start))
 
 	}()
 
